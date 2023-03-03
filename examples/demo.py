d30386812231c3471fc351f3b23aa0cf657ff601
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2023-03-03 13:49:12 +0100

    Update demo.py
    revert reordering

--- examples/demo.py
+++ examples/demo.py
@@ -1,18 +1,14 @@
 import datetime as dt
 from pdm_tools import tools
 
+sql = 'SELECT TOP(1) * FROM PDMVW.WELL_PROD_DAY'
+df = tools.query(sql, verbose=False)
+print(df)
+
 sql = "SELECT top(100) * FROM PDMVW.WELL_PROD_DAY WHERE COUNTRY = :countrycode AND PROD_DAY = :startdate"
 df = tools.query(sql, params={
     'countrycode': 'NO',
     'startdate': dt.datetime(2022,1,1)
     }
 )
-print(df)
-
-sql = 'SELECT TOP(1) * FROM PDMVW.WELL_PROD_DAY'
-df = tools.query(sql, verbose=False)
-print(df)
-
-sql = 'SELECT TOP(1) * FROM PDMVW.WELL_PROD_DAY'
-df = tools.query(sql, verbose=False)
 print(df)

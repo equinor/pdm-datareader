6e8765a884262f6906117e0d8261f6dc46d56c36
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2024-01-23 12:00:37 +0100

    refact: autoformatting, fix ruff warnings

--- examples/demo.py
+++ examples/demo.py
@@ -2,12 +2,13 @@ import datetime as dt
 
 from pdm_tools import tools
 
-sql = 'SELECT TOP(1) * FROM PDMVW.WELL_PROD_DAY'
+sql = "SELECT TOP(1) * FROM PDMVW.WELL_PROD_DAY"
 df = tools.query(sql)
 print(df)
 
 # Use parameter bindings to avoid SQL injection issues.
 sql = "SELECT top(100) * FROM PDMVW.WELL_PROD_DAY WHERE COUNTRY = :countrycode AND PROD_DAY = :startdate"
-df = tools.query(sql, params={'countrycode': 'NO',
-                 'startdate': dt.datetime(2022, 1, 1)})
+df = tools.query(
+    sql, params={"countrycode": "NO", "startdate": dt.datetime(2022, 1, 1)}
+)
 print(df)
